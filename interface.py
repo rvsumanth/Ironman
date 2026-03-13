69e070b8696f36f63701133509f4c7e65d4c8d11
venkatasumanthrayasam <venkatasumanthrayasam@gmail.com>
2026-03-13 21:34:37 +0530

    Optimized the Flow of Program

--- interface.py
+++ interface.py
@@ -3,41 +3,49 @@ from Access import *
 
 
 auth = Authentication()
-acc = suite_access_protection()
+int_acc = suite_access_protection()
+print('\nHello Welcome to VS Technology')
 while True:
-    print('Hello Welcome')
-    #section fro creating password
-    first_acc = int(input("Firstime accessing? choose 1 to Create Password or Already a user? choose any number to Login: "))
+    print()
+    #section for creating password
+    first_acc = int(input("If You are New User Choose 1 to Create Password\nIf you Forgot Your Password Choose 2 to Update\nIf you are already a user? choose any number to Login\nEnter Your Option:  "))
     if first_acc == 1:
-        cr_pass = acc.create_pass()
-        if cr_pass : 
-            print('Password Created!')
-        else :
-            print('Password Creation Denied! Retry it')
-
+        cr_pass = int_acc.create_pass()
+        if cr_pass != True:
+            continue
+    
+    #section for Updating Password
+    elif first_acc == 2:
+        updt_pass = int_acc.update_pass()
+        if updt_pass != True:
+            continue
+    
+    #Section for Login
     else:
-        #Section for Login
-        access_pass = input("Hello Enter Password to Access Interface: ")
-        if auth.interface_access(access_pass):
-            print("Access Granted")
-        else:
-            print("Access Denied")
-            #section for Forgot password
-            forgot_pass = int(input("Forgot Password? Don't worry Choose 1 for Update Password or choose any number to login: "))
-            if forgot_pass == 1:
-                up_pass = acc.update_pass()
-                if up_pass :
-                    print('Updated Successfully')
+         while True:
+            #interface login
+            login_int = auth.interface_access()
+            if login_int:
+                print("Welcome Sir select Operations ")
+                break
+            else:
+                for_pass = int(input('Forgot Password ? Choose 1 : '))        
+                if for_pass == 1: 
+                    break                    
                 else:
-                    print('Password Updation Denied! Retry with new one')
-            else: continue
+                    continue
+    
+            
         
-        print('Choose Operations')
-        op = input('What you need to do ?: ')
 
-        if op is None:
-            break
-        else: continue
+
+        
+        # print('Choose Operations')
+        # op = input('What you need to do ?: ')
+
+        # if op is None:
+        #     break
+        # else: continue
                 
 
 
