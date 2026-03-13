e7278c6bff29cef5e5279d467efdb278f4a0bf73
venkatasumanthrayasam <venkatasumanthrayasam@gmail.com>
2026-03-13 21:35:53 +0530

    Added Floder for daily prccess tracking

---
+++ testing_codes/Removed_codes.py
@@ -0,0 +1,48 @@
+#Removed Codes 
+# n = suite_access_protection()
+# while True:
+   
+#     a = int(input('1.create pass, 2.Update password'))
+#     if a == 1:
+        
+#         print("Default password:",n.interface_pass)
+#         user_data = input("create a new password: ")
+#         n.create_pass(user_data)
+#         print('Password Created') 
+#         print("display new password:",n.get_pass())
+        
+    
+#     elif a == 2:
+        
+#         print("Default password:",n.interface_pass)
+#         update_data = input("Enter a password to update: ")
+#         n.update_pass(update_data)
+#         print("Password Updated")
+#         print("Updated password:",n.get_pass())
+        
+#     else:
+#         print('Invalid Option')
+
+'''
+13-03-26:
+Situation: code modified . replaced below section and wrote new code section 
+Removed codes:
+
+        # 
+        # access_pass = input("Hello Enter Password to Access Interface: ")
+                
+        # if auth.interface_access(access_pass):
+        #     print("Access Granted")
+        # else:
+        #     print("Access Denied")
+        #     #section for Forgot password
+        #     forgot_pass = int(input("Forgot Password? Don't worry Choose 1 for Update Password or choose any number to login: "))
+        #     if forgot_pass == 1:
+        #         up_pass = acc.update_pass()
+        #         if up_pass :
+        #             print('Updated Successfully')
+        #             #need to wirte for  redirect to login function
+        #         else:
+        #             print('Password Updation Denied! Retry with new one')
+        #     else: continue
+'''
